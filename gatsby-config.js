8f64f4a2a000cd188909efd26c276469feecbcf6
Andre <asf@mail.usf.edu>
2021-02-01 17:17:04 -0500

    new changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,8 +28,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Simplefolio`,
-        short_name: `Simplefolio`,
+        name: `Andre Freeman Portfolio`,
+        short_name: `Portfolio`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#02aab0`,
